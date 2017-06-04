2a48bff3200a48096f9947b4bfed5a6f221bc88d
Matthew Roseman <mroseman95@gmail.com>
2017-06-04 19:06:33 -0400

    contact cards are now TouchableOpacity components, and can be pressed
    Signed-off-by: Matthew Roseman <mroseman95@gmail.com>

--- index.android.js
+++ index.android.js
@@ -9,20 +9,33 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
+  TouchableOpacity,
   Text,
   View
 } from 'react-native';
+
+class BusinessCardElement extends Component {
+    _onPressCard() {
+        console.log('test')
+    }
+    // TODO make button take up its parent view, and same color
+    render() {
+        return (
+            <ElevatedView elevation={3} style={styles.stayElevated}>
+                <TouchableOpacity style={styles.cardButton} onPress={this._onPressCard}>
+                    <Text>{this.props.cardName}</Text>
+                </TouchableOpacity>
+            </ElevatedView>
+        );
+    }
+}
 
 class BusinessCardList extends Component {
     render() {
         return (
             <View style={styles.businessCardContainer}>
-                <ElevatedView elevation={3} style={styles.stayElevated}>
-                    <Text>business card 1</Text>
-                </ElevatedView>
-                <ElevatedView elevation={3} style={styles.stayElevated}>
-                    <Text>business card 2</Text>
-                </ElevatedView>
+                <BusinessCardElement cardName='business card 1'/>
+                <BusinessCardElement cardName='business card 2'/>
             </View>
         );
     }
@@ -42,13 +55,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
+
   stayElevated: {
     width: 300,
     height: 50,
     margin: 10,
     backgroundColor: 'white',
+    alignItems: 'stretch',
+    justifyContent: 'center',
+  },
+
+  cardButton: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'white',
   }
 });
 
