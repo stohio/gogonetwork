4df4d87e7a48d694238113a3a3b8b3483eee03d5
Matthew Roseman <mroseman95@gmail.com>
2017-06-05 00:05:15 -0400

    added a add business card button
    Signed-off-by: Matthew Roseman <mroseman95@gmail.com>

--- app/scenes/MainScene.js
+++ app/scenes/MainScene.js
@@ -3,9 +3,11 @@ import ElevatedView from 'react-native-elevated-view';
 import StackNavigator from 'react-navigation';
 import {
     TouchableOpacity,
+    Button,
     StyleSheet,
     Text,
-    View
+    View,
+    ScrollView,
 } from 'react-native';
 
 class BusinessCardElement extends Component {
@@ -26,11 +28,44 @@ class BusinessCardElement extends Component {
 class BusinessCardList extends Component {
     render() {
         return (
-            <View style={styles.businessCardContainer}>
-                <BusinessCardElement cardName='business card 1' navigation={ this.props.navigation }/>
-                <BusinessCardElement cardName='business card 2' navigation={ this.props.navigation }/>
+            <View flex={8}>
+                <ScrollView flex={1} contentContainerStyle={styles.businessCardContainer}>
+                    <BusinessCardElement cardName='business card 1' navigation={ this.props.navigation }/>
+                    <BusinessCardElement cardName='business card 2' navigation={ this.props.navigation }/>
+                    <BusinessCardElement cardName='business card 3' navigation={ this.props.navigation }/>
+                    <BusinessCardElement cardName='business card 4' navigation={ this.props.navigation }/>
+                    <BusinessCardElement cardName='business card 5' navigation={ this.props.navigation }/>
+                    <BusinessCardElement cardName='business card 6' navigation={ this.props.navigation }/>
+                    <BusinessCardElement cardName='business card 7' navigation={ this.props.navigation }/>
+                    <BusinessCardElement cardName='business card 8' navigation={ this.props.navigation }/>
+                    <BusinessCardElement cardName='business card 9' navigation={ this.props.navigation }/>
+                    <BusinessCardElement cardName='business card 10' navigation={ this.props.navigation }/>
+                    <BusinessCardElement cardName='business card 11' navigation={ this.props.navigation }/>
+                    <BusinessCardElement cardName='business card 12' navigation={ this.props.navigation }/>
+                    <BusinessCardElement cardName='business card 13' navigation={ this.props.navigation }/>
+                    <BusinessCardElement cardName='business card 14' navigation={ this.props.navigation }/>
+                    <BusinessCardElement cardName='business card 15' navigation={ this.props.navigation }/>
+                    <BusinessCardElement cardName='business card 16' navigation={ this.props.navigation }/>
+                    <BusinessCardElement cardName='business card 17' navigation={ this.props.navigation }/>
+                    <BusinessCardElement cardName='business card 18' navigation={ this.props.navigation }/>
+                    <BusinessCardElement cardName='business card 19' navigation={ this.props.navigation }/>
+                </ScrollView>
             </View>
         );
+    }
+}
+
+class AddBusinessCardButton extends Component {
+    render() {
+        return (
+            <View style={styles.addBusinessCardButtonForm}>
+                <TouchableOpacity style={styles.addBusinessCardButton} onPress={() => {
+                        console.log('test');
+                    }}>
+                    <Text>Add Business Card</Text>
+                </TouchableOpacity>
+            </View>
+        ); 
     }
 }
 
@@ -41,15 +76,16 @@ export default class MainScreen extends Component {
 
     render() {
         return (
-            <BusinessCardList navigation={ this.props.navigation }/>
+            <View flex={1}>
+                <BusinessCardList navigation={ this.props.navigation }/>
+                <AddBusinessCardButton/>
+            </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
     businessCardContainer: {
-        flex: 1,
-        flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'flex-start',
         backgroundColor: '#F5FCFF',
@@ -70,4 +106,15 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         backgroundColor: 'white',
     },
+
+    addBusinessCardButtonForm: {
+        flex: 1,
+    },
+
+    addBusinessCardButton: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#458588',
+    }
 });
